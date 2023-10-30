763643306e5ca886b268419b7479a564fcbf5898
RoopakPalakurty <116860540+RoopakPalakurty@users.noreply.github.com>
2023-10-30 16:30:42 +0530

    Add files via upload

---
+++ class(2).py
@@ -0,0 +1,15 @@
+mylist=["banana","player",46,True]
+thislist=["hello"]
+mylist.append("orange")
+print(mylist)
+mylist.insert(5,"Bun")
+print(mylist)
+mylist.reverse()
+print(mylist)
+mylist.pop(1)
+print(mylist)
+mylist.extend(thislist)
+print(mylist)
+mylist.clear()
+print(mylist)
+del mylist
